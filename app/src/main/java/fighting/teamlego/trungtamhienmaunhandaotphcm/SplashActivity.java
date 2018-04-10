268b0c9b6625daa3931ab15c2555c497b6b316c2
huy031197 <33492436+huy031197@users.noreply.github.com>
2018-04-10 17:58:00 +0700

    SplashScreen

--- app/src/main/java/fighting/teamlego/trungtamhienmaunhandaotphcm/SplashActivity.java
+++ app/src/main/java/fighting/teamlego/trungtamhienmaunhandaotphcm/SplashActivity.java
@@ -1,7 +1,45 @@
 package fighting.teamlego.trungtamhienmaunhandaotphcm;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends MainActivity {
-    private int SPLASH_TIMER=2500;
+    protected boolean _active = true;
+    protected int _splashTime = 5000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // thread for displaying the SplashScreen
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while(_active && (waited < _splashTime)) {
+                        sleep(100);
+                        if(_active) {
+                            waited += 100;
+                        }
+                    }
+                } catch(InterruptedException e) {
+                    // do nothing
+                } finally {
+                    finish();
+                    Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                    SplashActivity.this.startActivity(mainIntent);
+                    SplashActivity.this.finish();
+                }
+            }
+        };
+        splashTread.start();
+    }
 
 
 }
